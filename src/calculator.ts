b4a4e7d1488e4190f98372978f7d08cee868be0d
qusay99 <qusay.hamid9@gmail.com>
2022-02-15 22:17:42 +0100

    fixed issues after testing the module

--- src/calculator.ts
+++ src/calculator.ts
@@ -5,53 +5,53 @@ export class GeometryCalculator{
         });
     }
     public static rectangle(a: number, b: number){
-        let perimeter = 2*(a+b);
-        let surface = a*b;
+        let perimeter = (2*(a+b)).toFixed(3);
+        let surface = (a*b).toFixed(3);
         let result: string = "Perimeter:\t" + perimeter + "\tSurface:\t" + surface;
 
         return result
     }
     public static square(a: number){
-        let perimeter = 4*a;
-        let surface = Math.pow(a,2);
+        let perimeter = (4*a).toFixed(3);
+        let surface = (Math.pow(a,2)).toFixed(3);
         let result: string = "Perimeter:\t" + perimeter + "\tSurface:\t" + surface;
 
         return result
     }
     public static parallelogram(a: number, b: number, ha: number){
-        let perimeter = 2*(a+b);
-        let surface = a*ha;
+        let perimeter = (2*(a+b)).toFixed(3);
+        let surface = (a*ha).toFixed(3);
         let result: string = "Perimeter:\t" + perimeter + "\tSurface:\t" + surface;
 
         return result
     }
     public static cube(a: number){   
-        let surface = 6*(Math.pow(a,2));
-        let volume = Math.pow(a,3);
+        let surface = (6*(Math.pow(a,2))).toFixed(3);
+        let volume = (Math.pow(a,3)).toFixed(3);
 
         let result: string = "Surface:\t" + surface + "\t Volume: \t" + volume;
 
         return result
     }
     public static cuboid(a: number, b: number, c: number){
-        let surface = 2*((a*b)+(a*c)+(b*c));
-        let volume = a*b*c;
+        let surface = (2*((a*b)+(a*c)+(b*c))).toFixed(3);
+        let volume = (a*b*c).toFixed(3);
 
         let result: string = "Surface:\t" + surface + "\t Volume: \t" + volume;
 
         return result
     }
     public static cylinder(r: number, h: number){
-        let surface = 2*Math.PI*r*(r+h);
-        let volume = Math.PI*(r**2)*h;
+        let surface = (2*Math.PI*r*(r+h)).toFixed(3);
+        let volume = (Math.PI*(r**2)*h).toFixed(3);
 
         let result: string = "Surface:\t" + surface + "\t Volume: \t" + volume;
 
         return result
     }
     public static sphere(r: number){
-        let surface = 4*Math.PI*r;
-        let volume = (4/3)*Math.PI*(r**2);
+        let surface = (4*Math.PI*(r**2)).toFixed(3);
+        let volume = ((4/3)*Math.PI*(r**3)).toFixed(3);
 
         let result: string = "Surface:\t" + surface + "\t Volume: \t" + volume;
 
--- test.ts
+++ test.ts
@@ -3,35 +3,35 @@ import {GeometryCalculator} from "./src/calculator.ts";
 
 Deno.test("Rectangle", () => {
     const result = GeometryCalculator.rectangle(4,6)
-    // assertEquals(result, "Surface:\t96\t Volume: \t64");
+    assertEquals(result, "Perimeter:\t20.000\tSurface:\t24.000")
 });
 
 Deno.test("Square", () => {
-    const result = GeometryCalculator.square(4)
-    // assertEquals(result, "Surface:\t96\t Volume: \t64");
+    const result = GeometryCalculator.square(5)
+    assertEquals(result, "Perimeter:\t20.000\tSurface:\t25.000")
 });
 
 Deno.test("Parallelogram", () => {
-    const result = GeometryCalculator.parallelogram(4,6,5)
-    // assertEquals(result, "Surface:\t96\t Volume: \t64");
+    const result = GeometryCalculator.parallelogram(4,6,3)
+    assertEquals(result, "Perimeter:\t20.000\tSurface:\t12.000")
 });
 
 Deno.test("Cuboid", () => {
-    const result = GeometryCalculator.cuboid(4)
-    // assertEquals(result, "Surface:\t96\t Volume: \t64");
+    const result = GeometryCalculator.cuboid(2,4,5)
+    assertEquals(result, "Surface:\t76.000\t Volume: \t40.000");
 });
 
 Deno.test("Cube", () => {
     const result = GeometryCalculator.cube(4)
-    assertEquals(result, "Surface:\t96\t Volume: \t64");
+    assertEquals(result, "Surface:\t96.000\t Volume: \t64.000");
 });
 
 Deno.test("Cylinder", () => {
-    const result = GeometryCalculator.cylinder(4)
-    // assertEquals(result, "Surface:\t96\t Volume: \t64");
+    const result = GeometryCalculator.cylinder(3,5)
+    assertEquals(result, "Surface:\t150.796\t Volume: \t141.372");
 });
 
 Deno.test("Sphere", () => {
-    const result = GeometryCalculator.sphere(4)
-    // assertEquals(result, "Surface:\t96\t Volume: \t64");
+    const result = GeometryCalculator.sphere(8)
+    assertEquals(result, "Surface:\t804.248\t Volume: \t2144.661");
 });
