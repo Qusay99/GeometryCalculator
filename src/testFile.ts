3e5702394a8a807b1596b23a46b9052c778b504b
Qusay99 <qusay.hamid9@gmail.com>
2022-02-15 12:39:30 +0100

    modified the code in calculator.ts

--- src/testFile.ts
+++ src/testFile.ts
@@ -1,13 +1,5 @@
-// console.log("Welcome to Deno!22");
-// console.log("Warum funktioniert das nicht?11");
-import {GeometryCalculator} from './calculator.ts';
-
-console.log(GeometryCalculator.rectangle(5,4));
-
-// function FindAllMethods(obj: any) {
-//     return Object.getOwnPropertyNames(obj).filter(function(property) {
-//         return typeof obj[property] == "function";
-//     });
-// }
+import {GeometryCalculator} from 'https://deno.land/x/geometry_calculator@v0.0.4/mod.ts';
 
 console.log(GeometryCalculator.availableMethods(GeometryCalculator));
+console.log(GeometryCalculator.rectangle(5,4));
+console.log(GeometryCalculator.cuboid(6,3,5));
