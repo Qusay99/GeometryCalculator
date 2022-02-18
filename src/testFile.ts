e3a5269e964b40bf7b00ccda6ed29d20105a967a
QusayH <36747388+Qusay99@users.noreply.github.com>
2022-02-19 00:33:36 +0100

    Update testFile.ts

--- src/testFile.ts
+++ src/testFile.ts
@@ -1,4 +1,4 @@
-import {GeometryCalculator} from 'https://deno.land/x/geometry_calculator@v0.0.6/mod.ts';
+import {GeometryCalculator} from 'https://deno.land/x/geometry_calculator@v1.0.3/mod.ts';
 
 console.log(GeometryCalculator.availableMethods(GeometryCalculator));
 console.log(GeometryCalculator.rectangle(5,4));
